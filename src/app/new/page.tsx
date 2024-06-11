da39bd70da21488847cf16de2311b7cce05c392e
F. T. Costa <47283591+CasualCosta@users.noreply.github.com>
2024-06-11 08:50:35 -0300

    Completed but data doesn't persist

---
+++ src/app/new/page.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import React from 'react'
+import prisma from '../db'
+import { redirect } from 'next/navigation'
+import { RedirectType } from 'next/navigation'
+import { RedirectStatusCode } from 'next/dist/client/components/redirect-status-code'
+
+async function createTodo(data: FormData){
+    "use server"
+    const title = data.get('title')?.valueOf()
+    if(typeof title !== 'string' || title.length === 0){
+        throw new Error("Invalid Title")
+    }
+
+    await prisma.todo.create({data: {title, complete: false}})
+    redirect('/')
+}
+
+const Page = () => {
+  return (
+    <div>
+        <header className="flex justify-between items-center mb-4">
+            <h1 className="text-2xl">Todos</h1>
+        </header>
+        <form action={createTodo} className='flex gap-2 flex-col'>
+            <label htmlFor="new-todo">What needs to be done?</label>
+            <input 
+                autoFocus
+                type="text" 
+                name="title" 
+                id="new-todo"
+                className='border border-slate-300 bg-transparent rounded px-2 py-1 mr-2 focus-within:border-slate-100 focus-within:ring-1 outline-none'
+            />
+            <div className='flex gap-1 justify-end'>
+                <Link href={'..'} className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>Cancel</Link>
+                <button type="submit" className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>Create</button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import prisma from "./db";
+import TodoItem from "@/components/TodoItem";
+
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  console.log(id, complete)
+  await prisma.todo.update({where: {id}, data: {complete: !complete}})
+}
+
+export default async function Home () {
+  const todos = await getTodos()
+  // await prisma.todo.create({ data: { title: "Teste", complete: false} })
+  return (
+    <div>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link href={"/new"} className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </div>
+  )
+}
